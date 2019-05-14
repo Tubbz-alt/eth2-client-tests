b0c026a3e53826dcbe12c5d7abb105b8881364ad
Antoine Toulme <antoine@lunar-ocean.com>
2019-05-14 12:39:50 -0700

    Move to our own Artemis docker image

--- tester/genesis/images.go
+++ tester/genesis/images.go
@@ -2,7 +2,7 @@ package genesis
 
 // mapping of blockchain to images
 var Images = map[string][]string{
-	"artemis":    []string{"gcr.io/whiteblock/artemis"},
+	"artemis":    []string{"artemis"},
 	"lighthouse": []string{"lighthouse"},
 	"prysm":      []string{"prysm"},
 }
